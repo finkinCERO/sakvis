f037f54f0a9125e746334c673423ae4caa3c77b8
finkin <davis.frank@satis-information.de>
2021-06-29 03:08:21 +0200

    123

--- songsWS/src/main/java/htwb/ai/SpringBootHelloWorldApplication.java
+++ songsWS/src/main/java/htwb/ai/SpringBootHelloWorldApplication.java
@@ -20,6 +20,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -37,7 +38,7 @@ import htwb.ai.model.SongList;
 @SpringBootApplication(scanBasePackages = {"htwb.ai"}, exclude = JpaRepositoriesAutoConfiguration.class)
 @EnableJpaRepositories({"htwb.ai.repository"})
 //@ComponentScan(basePackages= {"htwb.ai.*"})
-public class SpringBootHelloWorldApplication {
+public class SpringBootHelloWorldApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootHelloWorldApplication.class, args);
@@ -56,7 +57,58 @@ public class SpringBootHelloWorldApplication {
     @Autowired
     WebSecurityConfig web;
 
-    
+    @Override
+    public void run(String... args) throws Exception {
+
+        Users user1 = new Users("mmuster", "pass1234", "Max", "Muster");
+        user1.setPassword(web.passwordEncoder().encode(user1.getPassword()));
+        Users user2 = new Users("eschuler", "pass1234", "Elena", "Schuler");
+        user2.setPassword(web.passwordEncoder().encode(user2.getPassword()));
+//
+        List<Song> songs = readJSONToSongs("src/main/resources/songs.json");
+
+        List<SongList> songlistjson = readJSONToSongList("src/main/resources/songList.json");
+
+        int i = 1;
+        for (SongList st : songlistjson) {
+                if(i==1 || i==2) st.setOwner(user1);
+                else st.setOwner(user2);
+                i++;
+        }
+        songlistjson.get(0).setIsPrivate(true);
+        songlistjson.get(1).setIsPrivate(false);
+        songlistjson.get(2).setIsPrivate(true);
+        songlistjson.get(3).setIsPrivate(false);
+
+
+        // 1
+
+        //user.songlist1.setPassword(bcrypt);
+
+//		songRepository.saveAll(songs);
+        //this.userRepository.save(user1);
+        //this.userRepository.save(user2);
+
+        songlistRepository.saveAll(songlistjson);
+
+        SongList sl1 = songlistRepository.getById(1);
+
+        SongList songlist1 = new SongList("Forro music", false, user1);
+        SongList songlist2 = new SongList("Workout music", true, user1);
+
+        SongList songlist3 = new SongList("Rock and roll", true, user2);
+        SongList songlist4 = new SongList("Salsa", false, user2);
+
+        user1.getSongLists().add(songlist1);
+        user1.getSongLists().add(songlist2);
+
+        user2.getSongLists().add(songlist3);
+        user2.getSongLists().add(songlist4);
+
+        this.userRepository.save(user1);
+        this.userRepository.save(user2);
+
+    }
 
     @SuppressWarnings("unchecked")
     public static List<Song> readJSONToSongs(String filename) throws FileNotFoundException, IOException {
